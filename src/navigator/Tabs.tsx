ebfb8470ca4fd185ced5febbdc4a11f0a8d478de
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-06-02 12:09:59 -0400

    se agrego un cambio en el icono de tab

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -5,6 +5,7 @@ import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 import { StackNavigator } from './StackNavigator';
 import { colors } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,7 +18,30 @@ export const Tabs=()=> {
         borderTopColor:colors.primary,
         borderTopWidth:0,
         elevation:0,
-    }}}
+    },
+    labelStyle:{
+        fontSize:15
+    }
+}}
+                     
+    screenOptions={({route})=>({
+                    //props
+        tabBarIcon:({color,focused,size})=>{
+            let iconName :string=''
+            switch(route.name){
+                case 'Tab1Screen':
+                    iconName='T1'
+                    break;
+                case 'Tab2Screen':
+                    iconName='T2'
+                    break;
+                case 'StackNavigator':
+                    iconName='Stk'
+                    break;
+            }
+            return <Text>{iconName}</Text>
+        }
+    })}
 
     >
       <Tab.Screen name="Tab1Screen" options={{title:"tab1"}} component={Tab1Screen} />
